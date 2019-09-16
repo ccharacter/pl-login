72dad374b2b335dc46680c00eb7ebdd3a68d0e1b
Sharon Stromberg <sharon@ccharacter.com>
2019-09-16 17:21:21 -0400

    Fix misplaced function

--- sws-login-blocker.php
+++ sws-login-blocker.php
@@ -4,7 +4,7 @@
  * Plugin Name:       SWS Login Blocker
  * Plugin URI:        https://github.com/ccharacter/pl-login
  * Description:       Redirect users away from login page based on IP address
- * Version:           1.17
+ * Version:           1.18
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Sharon Stromberg
@@ -67,22 +67,5 @@ function sws_login_limiter() {
 add_action( 'login_enqueue_scripts', 'sws_login_limiter');
 
 
-
-	private function get_repository_info() {
-	  if ( is_null( $this->github_response ) ) { // Do we have a response?
-		$request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository ); // Build URI
-		if( $this->authorize_token ) { // Is there an access token?
-			$request_uri = add_query_arg( 'access_token', $this->authorize_token, $request_uri ); // Append it
-		}        
-		$response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri ) ), true ); // Get JSON and parse it
-		if( is_array( $response ) ) { // If it is an array
-			$response = current( $response ); // Get the first item
-		}
-		if( $this->authorize_token ) { // Is there an access token?
-			$response['zipball_url'] = add_query_arg( 'access_token', $this->authorize_token, $response['zipball_url'] ); // Update our zip url with token
-		}
-		$this->github_response = $response; // Set it to our property  
-	  }
-	}
 
 ?>
