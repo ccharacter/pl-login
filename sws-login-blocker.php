a8e310fb0416e0ef06e4c32b6de2a97e094f1336
Sharon Stromberg <sharon@ccharacter.com>
2020-08-04 23:21:51 -0400

    v3.1

--- sws-login-blocker.php
+++ sws-login-blocker.php
@@ -4,7 +4,7 @@
  * Plugin Name:       SWS Login Blocker
  * Plugin URI:        https://ccharacter.com/custom-plugins/sws-login-blocker/
  * Description:       Redirect users away from login page based on IP address
- * Version:           3.0
+ * Version:           3.1
  * Requires at least: 5.2
  * Requires PHP:      5.2
  * Author:            Sharon Stromberg
@@ -27,10 +27,25 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 );
 
 
-
-
-
 $myLoginBlocker = new swsLoginBlocker();
 $myLoginBlocker->init();
+
+
+// Run this filter with priority 9999 (last, or close to last), after core WP filters have run
+add_filter('authenticate', 'sws_reject_users', 9999, 3);
+
+// Your custom filter function
+function sws_reject_users( $user, $username, $password) {
+    $src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSi8u5A4bYyjXKw1GZ7N9d_kUnf5SOt0uqgS42sUa-CYjxYQBpA1b_XFczK0i-cmzr7PQ5dRmVbvGCE/pub?output=csv";
+    $redirect_users = sws_login_blocker_csvToArray($src,",","N");
+
+    // If the user is in the array of users to redirect, then send them away
+    if ( in_array( $username, $redirect_users ) ) {
+        header("location: /bad-user");
+        die();
+    }
+
+    return $user;
+}
 
 ?>
